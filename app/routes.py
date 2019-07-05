4a44f471c0ccffa911f6e4d4da25d8839b2e2338
Rishabh  Sarup <rs868@cornell.edu>
2019-07-05 18:29:40 +0800

    index.html styling

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {"name" : "Rishabh Sarup"}
-    render_template('index.html', title = "Home", user=user)
+    return render_template('index.html', title = "Home", user=user)
