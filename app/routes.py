0e38533eb680b14b5e8e0ca78e34baa7d473d073
Rishabh  Sarup <rs868@cornell.edu>
2019-07-07 16:44:00 +0800

    login page and redirects temp

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
 
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {"username" : "Rishabh Sarup"}
-    return render_template('index.html', title = "Home", user=user)
+    # user = {"username" : "Rishabh Sarup"}
+    return render_template('index.html', title = "Index")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -15,5 +15,10 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('home'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/home')
+def home():
+    user = {"username": "Rishabh Sarup"}
+    return render_template('home.html', title = "Home", user=user)
