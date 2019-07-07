fb098b968511fe5edfbfbdb45b05fad8f008d0ae
Rishabh  Sarup <rs868@cornell.edu>
2019-07-07 18:24:20 +0800

    db tables and relations created and migrated

--- app/models.py
+++ app/models.py
@@ -7,10 +7,15 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    bankBalance = db.Column(db.Integer)
-    bankBalance = db.Column(db.Integer)
-    payappBalance = db.Column(db.Integer)
+    cashBalance = db.Column(db.Integer, index = True)
+    bankBalance = db.Column(db.Integer, index = True)
+    payappBalance = db.Column(db.Integer, index = True)
     password_hash = db.Column(db.String(128))
+
+    #creating relations between user and transactions
+    allCashTransactions = db.relationship('CashTransaction', backref = 'cashspender', lazy = 'dynamic')
+    allBankTransactions = db.relationship('BankTransaction', backref= 'bankspender', lazy = 'dynamic')
+    allPayAppTransactions = db.relationship('PayAppTransaction', backref = 'payappspender', lazy = 'dynamic' )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -18,29 +23,18 @@ class User(db.Model):
 #Class for all cash transactions that the user makes.
 class CashTransaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index = True, unique = True) #The user that the transaction is associated with.
-    debit = db.Column(db.Boolean) # True if the amount is to be subtracted, false if it is actually a credit
-    amount = db.Column(db.Integer)
-
-    def __repr(self):
-        return '<User {}>'.format(self.username) + str(id)
-
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #The user that the transaction is associated with.
+    debit = db.Column(db.Boolean, index = True) # True if the amount is to be subtracted, false if it is actually a credit
+    amount = db.Column(db.Integer, index = True)
+    
 class BankTransaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index = True, unique = True) #The user that the transaction is associated with.
-    debit = db.Column(db.Boolean) # True if the amount is to be subtracted, false if it is actually a credit
-    amount = db.Column(db.Integer)
-
-    def __repr(self):
-        return '<User {}>'.format(self.username) + str(id)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #The user that the transaction is associated with.
+    debit = db.Column(db.Boolean, index = True) # True if the amount is to be subtracted, false if it is actually a credit
+    amount = db.Column(db.Integer, index = True)
 
 class PayAppTransaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index = True, unique = True) #The user that the transaction is associated with.
-    debit = db.Column(db.Boolean) # True if the amount is to be subtracted, false if it is actually a credit
-    amount = db.Column(db.Integer)
-
-    def __repr(self):
-        return '<User {}>'.format(self.username) + str(id)
-
-        
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #The user that the transaction is associated with.
+    debit = db.Column(db.Boolean, index = True) # True if the amount is to be subtracted, false if it is actually a credit
+    amount = db.Column(db.Integer, index = True)
