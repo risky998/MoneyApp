b6f17917bd92141b1cb9361dfc756618dea38b71
Rishabh  Sarup <rs868@cornell.edu>
2019-07-17 23:15:52 +0800

    more password reset

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT  = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['rishabhsarup@gmail.com'] #can be editied to change the email admin at any time.
