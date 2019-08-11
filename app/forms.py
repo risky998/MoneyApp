3ac60a7111708193efcf14010eb968a0e1ebd735
Rishabh  Sarup <rs868@cornell.edu>
2019-08-11 18:46:00 +0800

    edited wording of transfer form:

--- app/forms.py
+++ app/forms.py
@@ -51,7 +51,7 @@ class StartingBalanceForm(FlaskForm):
 class TransactionForm(FlaskForm):
     date = StringField('Date - Enter dd/mm/yy', validators = [DataRequired()])
     # debit = BooleanField('Was this a debit?') #need to remove this field and adjust the rest of the app
-    transtype = SelectField ('Please select the type of this transaction', choices = [('debit', 'Debit'), ('receipt', 'Receipt'), ('banktopayapp', 'Own Bank Account to Own Pay App Account'), ('payapptobank', 'Own PayApp Account to Own Bank Account')])
+    transtype = SelectField ('Please select the type of this transaction', choices = [('debit', 'Expense'), ('receipt', 'Receipt'), ('banktopayapp', 'Transfer from Own Bank Account to Own Pay App Account'), ('payapptobank', 'Transfer from Own PayApp Account to Own Bank Account')])
     amount = DecimalField("Amount", validators = [DataRequired("Please Enter Numbers Only")])
     type = SelectField('Receipt/Payment Mode', choices = [('Cash', 'Cash'), ('Bank', 'Bank'), ('PayApp', 'PayApp')], validators = [DataRequired()])
     category = SelectField('Transaction Category', choices = [('Food and Beverage', 'Food and Beverage'), ('Transport', 'Transport'), ('Lifestyle', 'Lifestyle'), ('Other', 'Other')], validators = [DataRequired()])
