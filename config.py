cc843cfb667465bf7e44e5049b648ad27cb9cbd9
Rishabh  Sarup <rs868@cornell.edu>
2019-08-15 22:07:07 +0800

    heroky deployment changes

--- config.py
+++ config.py
@@ -13,3 +13,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['rishabhsarup@gmail.com'] #can be editied to change the email admin at any time.
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    
